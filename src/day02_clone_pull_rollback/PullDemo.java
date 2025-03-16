c431ddcd3177de89f0113dc3bc45e94096c7a467
esmiraaidinova <esmira.mikail@gmail.com>
2025-03-16 11:51:17 -0400

    Added line 13 local

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -1,6 +1,7 @@
 package day02_clone_pull_rollback;
 
 public class PullDemo {
+
     public static void main(String[] args) {
 
         System.out.println("Hello From Local");
@@ -8,5 +9,7 @@ public class PullDemo {
         System.out.println("Hello From another teammate in GitHub");
 
         System.out.println("GitHub i did another change again");
+
+        System.out.println("In Local Intellij");
     }
 }

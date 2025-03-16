5971c0b80c500a8bb68d83f389e6f076d5882d32
Esmira99 <esmira.mikail@gmail.com>
2025-03-16 11:25:08 -0400

    Update PullDemo.java

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -6,5 +6,7 @@ public class PullDemo {
         System.out.println("Hello From Local");
         System.out.println("Hello From teammate in GitHub");
         System.out.println("Hello From another teammate in GitHub");
+
+        System.out.println("GitHub i did another change again");
     }
 }
